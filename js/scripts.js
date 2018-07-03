4ee77c62c7b75691aa048bf4a8d56f6172e7c8c9
Emery Haller <dracofett@outlook.com>
2018-07-02 20:42:04 -0700

    end of day 1

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//decalare a variable
+var firstName;
+
+//define variable
+firstName = " Emery"
+
+//defoine and decalare
+var lastName = "Haller"
+
+console.log(firstName,lastName);
+
+
+
+firstName= "SuperMan"
+
+console.log(firstName);
+//SINGLE LINE COMMENT
+/*MULTILINE
+COMMENT*/
+
+{
+  //nEW SCOPE
+}
