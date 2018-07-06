72d6f5016957e0bac89027eff841f84b57e78cc8
Emery Haller <dracofett@outlook.com>
2018-07-05 20:50:49 -0700

    Day 2 full commit

--- js/scripts.js
+++ js/scripts.js
@@ -22,8 +22,36 @@ var myString = "Hello World!*76 ";
 console.log(typeof myString)
 
 var myNumber = 42;
+var myOtherNumber = new Number (42);
 
-console.log(typeof myNumber);
+console.log(myNumber, myOtherNumber);
 
 var newVariable;
-console.log(newVaria)
+console.log(newVariable )
+
+console.log("hey there, " + firstName);//es5
+console.log(`Hey There, ${lastName} How are you`);
+let newString = 'it Worked';
+console.log('after' , newString);
+{
+let newString = 'It didnt work';
+console.log('inside', newString)
+};
+newString = 'it almost worked'
+console.log('Outside', newString);
+
+const age = 18
+console.log(age )
+
+let today = new Date();
+console.log(today)
+
+let bDay = new Date('06-01-2000');
+console.log(bDay)
+
+//alert('hey do a thing')// 1direct COMMENT
+//const isReady = confirm ('hey person I do you like me ')
+//alert(isReady)
+
+let username = prompt(' wht is u Called', 'Boi');
+console.log(username)
