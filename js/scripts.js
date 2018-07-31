b3589ecb36bbbae929555dba3fb480bbe051bcf0
Emery Haller <dracofett@outlook.com>
2018-07-30 19:45:29 -0700

    day 8 done

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// let loginForm = document.forms.loginForm
+// let emailInput = loginForm.email
+// let passwordInput = loginForm.password;
+
+// emailInput.addEventListener("keydown", e => {
+//     console.log(e.target.value);
+//    // e.target.value //what is being typed
+// });
+
+// loginForm.addEventListener("submit", e => {
+//     e.preventDefault();
+//     console.log(e);
+//     loginForm.reset();
+// })
+// let favorites = ["His Girlfriend"]
+function listItems(){
+    console.log(this)
+    alert(`${this.name} loves ${this.favorites[0]}`)
+}
+//listItems();
+let user = {
+    name: "Emery",
+    cart: [],
+    coupons: [],
+    favorites: ['Brielle'],
+    listFavorites: listItems
+
+}
+
+//implicit this binding
+user.listFavorites();
+
+let jr = {
+    name: "Juniro kujo",
+    cart: ["platinum silver"],
+    coupons: ["star"],
+    favorites : ["joestar"],
+}
+
+listItems.call(jr);
